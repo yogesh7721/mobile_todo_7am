30bbe4beea7ac63979dd410e45b3bc1a4cc859a2
yogesh shelke <shelkeyogesh874@gmail.com>
2024-12-24 09:24:36 +0530

    changes

--- index.js
+++ index.js
@@ -8,19 +8,28 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin: "http://localhost:5173",
+    credentials: true
 }))
 
-app.use("/api/auth",require("./routes/auth.routes"))
-app.use("/api/admin",require("./routes/admin.routes"))
-app.use("/api/employee",require("./routes/employee.route"))
-app.use("*",(req,res)=>{
-    res.status(404).json({message:"resource not found"})
+app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/admin", require("./routes/admin.routes"))
+app.use("/api/employee", require("./routes/employee.route"))
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "resource not found" })
+})
+
+// express error handler
+app.use((err, req, res, next) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Something went wrong..." })
+
+    }
 })
 
 mongoose.connect(process.env.MONGO_URL)
-mongoose.connection.once("open",()=>{
+mongoose.connection.once("open", () => {
     console.log("mongo connect")
-    app.listen(process.env.PORT,console.log("server runnung"))
+    app.listen(process.env.PORT, console.log("server runnung"))
 })
