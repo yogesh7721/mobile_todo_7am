56aad06553e9310b5f746094d657a3c3f059b38d
yogesh shelke <shelkeyogesh874@gmail.com>
2024-12-24 10:24:01 +0530

    employee add

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,67 +4,71 @@ const jwt = require("jsonwebtoken")
 const Admin = require("../model/Admin")
 const Employee = require("../model/Employee")
 
-exports.adminRegister = asyncHandler(async(req,res) => {
-    const {password} = req.body
-    const hash = await bcrypt.hash(password,10)
-    await Admin.create({...req.body,password:hash})
-    res.json({message:"admin register success"})
+exports.adminRegister = asyncHandler(async (req, res) => {
+    const { password } = req.body
+    const hash = await bcrypt.hash(password, 10)
+    await Admin.create({ ...req.body, password: hash })
+    res.json({ message: "admin register success" })
 })
-exports.adminLogin = asyncHandler(async(req,res) => {
-    const {email,password} = req.body
-    const result = await Admin.findOne({email})
-    if(!result){
-       return res.status(401).json({message:"email not found"})
+exports.adminLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const result = await Admin.findOne({ email })
+    if (!result) {
+        return res.status(401).json({ message: "email not found" })
     }
-    const verify = await bcrypt.compare(password,result.password)
-    if(!verify){
-        return res.status(401).json({message:"password do not match"})
+    const verify = await bcrypt.compare(password, result.password)
+    if (!verify) {
+        return res.status(401).json({ message: "password do not match" })
     }
-    const token = jwt.sign({_id:result._id},process.env.JWT_SECRET,{expiresIn:"1d"})
-    res.cookie("todo-admin",token,{
-        maxAge:1000 * 60 * 60 * 24,
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "prod" ? true : false //keep false for local and true in live
+    const token = jwt.sign({ _id: result._id }, process.env.CLOUDINARY_API_SECRET, { expiresIn: "1d" })
+    res.cookie("todo-admin", token, {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "prod" ? true : false //keep false for local and true in live
     })
-    res.json({message:"admin login success",result:{
-        name:result.name,
-        email:result.email,
-    }})
+    res.json({
+        message: "admin login success", result: {
+            name: result.name,
+            email: result.email,
+        }
+    })
 })
-exports.adminLogout = asyncHandler(async(req,res) => {
+exports.adminLogout = asyncHandler(async (req, res) => {
     res.clearCoookie("todo-admin")
-    res.json({message:"admin logout success"})
+    res.json({ message: "admin logout success" })
 })
 
-exports.employeeRegister = asyncHandler(async(req,res) => {
-    const {name,email} = req.body
-    const password = name.slice(0,2) + email.slice(0,2)
-    const hash = await bcrypt.hash(password,10)
-    await Employee.create({...req.body,password:hash})
-    res.json({message:"employee register success"})
+exports.employeeRegister = asyncHandler(async (req, res) => {
+    const { name, email } = req.body
+    const password = name.slice(0, 2) + email.slice(0, 2)
+    const hash = await bcrypt.hash(password, 10)
+    await Employee.create({ ...req.body, password: hash })
+    res.json({ message: "employee register success" })
 })
-exports.employeeLogin = asyncHandler(async(req,res) => {
-    const {email,password} = req.body
-    const result = await Employee.findOne({email})
-    if(!result){
-        res.status(401).json({message:"email not found"})
+exports.employeeLogin = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
+    const result = await Employee.findOne({ email })
+    if (!result) {
+        res.status(401).json({ message: "email not found" })
     }
-    const verify = await bcrypt.compare(password,result.password)
-    if(!verify){
-        res.status(401).json({message:"password do not match"})
+    const verify = await bcrypt.compare(password, result.password)
+    if (!verify) {
+        res.status(401).json({ message: "password do not match" })
     }
-    const token = jwt.sign({_id:result._id},process.env.JWT_SECRET,{expiresIn:"1d"})
-    res.cookie("todo-employee",token,{
-        maxAge:1000 * 60 * 60 * 24,
-        httpOnly:true,
-        secure:process.env.NODE_ENV === "prod" ? true : false //keep false for local and true in live
+    const token = jwt.sign({ _id: result._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+    res.cookie("todo-employee", token, {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "prod" ? true : false //keep false for local and true in live
     })
-    res.json({message:"employee login success",result:{
-        name:result.name,
-        email:result.email,
-    }})
+    res.json({
+        message: "employee login success", result: {
+            name: result.name,
+            email: result.email,
+        }
+    })
 })
-exports.employeeLogout = asyncHandler(async(req,res) => {
+exports.employeeLogout = asyncHandler(async (req, res) => {
     res.clearCoookie("todo-employee")
-    res.json({message:"admin logout success"})
+    res.json({ message: "admin logout success" })
 })
--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -1,10 +1,11 @@
 const asyncHandler = require("express-async-handler")
+const Todo = require("../model/Todo")
 
-exports.readEmployeeTodo = asyncHandler(async(req,res)=>{
-    const result = await Todo.find({employee:req.employee})
-    res.json({message:"todo fetch success",result})
+exports.readEmployeeTodo = asyncHandler(async (req, res) => {
+    const result = await Todo.find({ Employee: req.employee })
+    res.json({ message: "todo fetch success", result })
 })
-exports.completeEmployeeTodo = asyncHandler(async(req,res)=>{
-    await Todo.findByIdAndUpdate(req.params.tid,{isComplete:true})
-    res.json({message:"todo complete success"})
+exports.completeEmployeeTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.tid, { isComplete: true })
+    res.json({ message: "todo complete success" })
 })
